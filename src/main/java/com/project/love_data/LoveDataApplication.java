23258722953b6d5c78e0cf73965330846d2fc418
ARA <mon0mon@naver.com>
2021-04-01 21:41:53 +0900

    NEW START (2021-04-01) ################
    Spring Boot 2.4.4
    JDK 1.8
    
    Dependancies
    - jdbc
    - oauth2
    - spring boot security
    - thymeleaf
    - web
    - lombok
    - mysql connector
    - restful

---
+++ src/main/java/com/project/love_data/LoveDataApplication.java
@@ -0,0 +1,13 @@
+package com.project.love_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/love_data/LoveDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.love_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
