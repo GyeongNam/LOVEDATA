2235daabd3dd6d41a26b7cb92b408e4e71aa452a
LAPTOP-EHQJ4UCA\27251 <272518@naver.com>
2021-07-05 23:26:49 +0900

    마이페이지 날짜 셀렉트 박스에서 데이터피커로 수정
    마이페이지 겹치는 페이지 삭제

--- src/main/resources/static/js/mypage.js
+++ src/main/resources/static/js/mypage.js
@@ -44,7 +44,6 @@ $('.preview-edit').click( function() {
 
 //휴대폰번호 나누기
 $(document).ready(function () {
-    setDateBox();
     var no = $("#numnum").val();
     var fnum = no.substring(0,3);
     var snum = no.substring(3,7);
@@ -57,7 +56,6 @@ $(document).ready(function () {
 
 //생년월일 나누기
 $(document).ready(function () {
-    setDateBox();
     var mybusday = $("#mybir").val();
     var myyear = mybusday.substring(0,4);
     var mymon = mybusday.substring(5,7);
@@ -68,32 +66,38 @@ $(document).ready(function () {
     $("#day").val(myday);
 });
 
+//생년월일 나누기
+$(document).ready(function () {
+    var jd = $("#jender").val();
+    console.log(jd);
+    $("#year").val(myyear);
+});
 
 // select box 연도 , 월 표시
-function setDateBox() {
-    var dt = new Date();
-    var year = "";
-    var com_year = dt.getFullYear();
-
-    // 발행 뿌려주기
-    $("#year").append("<option value=''>년도</option>");
-
-    // 올해 기준으로 -50년부터 +1년을 보여준다.
-    for (var y = (com_year - 50); y <= (com_year + 1); y++) {
-        $("#year").append("<option value='" + y + "'>" + y + " 년" + "</option>");
-    }
-
-    // 월 뿌려주기(1월부터 12월)
-    var month;
-    $("#month").append("<option value=''>월</option>");
-    for (var i = 1; i <= 12; i++) {
-        $("#month").append("<option value='" + i + "'>" + i + "월" + "</option>");
-    }
-
-    // 일 뿌려주기(1일부터 31일)
-    var day;
-    $("#day").append("<option value=''>일</option>");
-    for (var i = 1; i <= 31; i++) {
-        $("#day").append("<option value='" + i + "'>" + i + " 일" + "</option>");
-    }
-}
+// function setDateBox() {
+//     var dt = new Date();
+//     var year = "";
+//     var com_year = dt.getFullYear();
+//
+//     // 발행 뿌려주기
+//     $("#year").append("<option value=''>년도</option>");
+//
+//     // 올해 기준으로 -50년부터 +1년을 보여준다.
+//     for (var y = (com_year - 50); y <= (com_year + 1); y++) {
+//         $("#year").append("<option value='" + y + "'>" + y + " 년" + "</option>");
+//     }
+//
+//     // 월 뿌려주기(1월부터 12월)
+//     var month;
+//     $("#month").append("<option value=''>월</option>");
+//     for (var i = 1; i <= 12; i++) {
+//         $("#month").append("<option value='" + i + "'>" + i + "월" + "</option>");
+//     }
+//
+//     // 일 뿌려주기(1일부터 31일)
+//     var day;
+//     $("#day").append("<option value=''>일</option>");
+//     for (var i = 1; i <= 31; i++) {
+//         $("#day").append("<option value='" + i + "'>" + i + " 일" + "</option>");
+//     }
+// }
